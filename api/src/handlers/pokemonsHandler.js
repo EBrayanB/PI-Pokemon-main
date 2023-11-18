ac3200fc27ffb3d8824abff370b0d73a93d0a14b
Brayan Bastidas <brayanbmx_595@live.com>
2023-11-17 19:32:30 -0500

    handlers

--- api/src/handlers/pokemonsHandler.js
+++ api/src/handlers/pokemonsHandler.js
@@ -1,50 +1,42 @@
 const {getAllPokemons,getPokemonById,getPokemonByName,postPokemon} = require('../controllers/pokemonsController')
 
-// Handler trae por nombre y ejecuta funcion del controlador 
-
-const getPokemonsHandler = async (req,res) => {
-    const {name} = req.query;
-    console.log(name);
+const getPokemonsHandler = async (req,res) =>{
+    const {name} = req.query
+    console.log(name)
     try {
-        if (name){
-            const res = await getPokemonByName(name);
-            return res.status(200).json(res);
+        if(name){
+            const response = await getPokemonByName(name)
+             return res.status(200).json(response)
         }
-        const res = await getAllPokemons()
-        res.status(200).json(res)
+        const response = await getAllPokemons()
+        res.status(200).json(response)
     } catch (error) {
         res.status(400).json({error: error.message})
     }
 }
 
-// Hanlder por id llamando a la funcion del controlador 
-
-const getPokemonByIdHandler = async(req,res)=>{
+const getPokemonByIdHandler = async (req,res)=>{
     const {id} = req.params;
     try {
-        const res = await getPokemonById(id);
-        res.status(200).json(res);
+        const response = await getPokemonById(id)
+        res.status(200).json(response)
     } catch (error) {
-        res.status(400).json({error:error.message});
+        res.status(400).json({error: error.message})
     }
 }
 
-// Handler que llama y ejecuta la funcion del controller post 
-
-const postPokemonHandler = async(req,res) =>{
-    const {name,img.hp,attack,defense,speed,heigth,weigth,type} = req.body;
+const postPokemonHandler = async (req,res) =>{
+    const {name,img,hp,attack,defense,speed,height,weight,type}= req.body
     try {
-        const res = await postPokemon(name,img.hp,attack,defense,speed,heigth,weigth,type);
-        res.status(2001).json(res);
+        const response = await postPokemon(name,img,hp,attack,defense,speed,height,weight,type)
+                res.status(201).json(response)
     } catch (error) {
-        res.status(400).json({error:error.message});
+        res.status(400).json({error: error.message})
     }
 }
 
 
-// Exporto 
-
-module.exports ={
+module.exports = {
     getPokemonsHandler,
     getPokemonByIdHandler,
     postPokemonHandler,
